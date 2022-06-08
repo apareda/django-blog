904a98869243a52baae11a48ef9d28ee8cc98098
Adrian Paredes <pareda@uw.edu>
2022-06-07 20:33:15 -0700

    update for github sign in

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,14 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
 
 urlpatterns = [
-    path("", include("blogging.urls")),
-    path("polling/", include("polling.urls")),  # <-- Add this
-    path("admin/", admin.site.urls),
-    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
-    path('accounts/', include('allauth.urls')), # new
+    path('admin/', admin.site.urls),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites', # new
+
+
+    'allauth', # new
+    'allauth.account', # new
+    'allauth.socialaccount', # new
+    'allauth.socialaccount.providers.github', # new
+
     "polling",
     "blogging",
 ]
@@ -120,6 +128,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 STATIC_URL = "/static/"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
+
+
